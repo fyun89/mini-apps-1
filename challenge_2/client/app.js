8da427fae802ccbf014a9f39a7f1c0ad70eea812
fyun89 <fyun89@gmail.com>
2018-05-22 15:58:53 -0700

    Basic setup complete

--- challenge_2/client/app.js
+++ challenge_2/client/app.js
@@ -1,2 +1,7 @@
 
-
+$(document).ready(function(){
+	$('form').on('submit', function(e){
+		e.preventDefault();
+		console.log('working')
+	})
+})
--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,15 @@
+const express = require('express'); //do I need version num here? (doc shows)
+const app = express();
 
+app.use(express.static('client'));
+
+app.get('/', (req, res) => {
+	console.log(req.url);
+	res.send('Hello World!');
+});
+
+app.get('/test', (req,res)=> {
+	res.send('HELLO');
+});
+app.listen(3000, () => console.log('Example app listening on port 3000!'))
 
