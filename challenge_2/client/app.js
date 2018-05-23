fb33fe2bd0cdc94390e0a5297b9bea91239b5efe
fyun89 <fyun89@gmail.com>
2018-05-22 21:45:38 -0700

    Server setup, recursion logic complete. WIP need to implement the logic to fully convert JSON to CSV and display the result in the site

--- challenge_2/client/app.js
+++ challenge_2/client/app.js
@@ -1,7 +1,19 @@
-
+//var server = require('../server.js')
 $(document).ready(function(){
 	$('form').on('submit', function(e){
 		e.preventDefault();
-		console.log('working')
-	})
-})
+		//console.log($('textarea').val())
+		
+		var data = $('textarea').val()
+
+		$.ajax({
+		  type: "POST",
+		  url: '/converter',
+		  contentType: 'applications/JSON',
+		  data: data,
+		  success: function(data) {console.log('success')},
+		  error: function(err) {console.log('error')}
+		  // dataType: 'json'
+		});
+	});
+});
--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,5 +1,8 @@
 const express = require('express'); //do I need version num here? (doc shows)
 const app = express();
+const bodyParser = require('body-parser')
+
+// var jsonParser = bodyParser.json()
 
 app.use(express.static('client'));
 
@@ -8,8 +11,44 @@ app.get('/', (req, res) => {
 	res.send('Hello World!');
 });
 
-app.get('/test', (req,res)=> {
-	res.send('HELLO');
-});
+// app.post('/converter', jsonParser, (req,res)=> {
+// 	// console.log(req)
+// 	console.log(req.body);
+// 	res.send('hello world');
+// });
+
+var csvConverter = function(data) {
+  //console.log(JSON.parse(data.slice(0,-1)))
+  var dataToConvert = [JSON.parse(data.slice(0,-1))]; //removes the semicolon at the end which caused error
+  //var columnName = [] //column names
+  var names = []; //data
+  var recursion = function(node) { //recurse to retrieve the data
+    var currNode = node || dataToConvert[0];
+    names.push(currNode.firstName);
+    if (currNode.children){
+      currNode.children.forEach(function(elem){
+    	recursion(elem);
+      });
+    }
+    //console.log(names)
+  }
+  recursion()
+  console.log(names)
+  //console.log(dataToConvert[0].firstName)
+}
+
+app.post('/converter', (req,res) => {
+	var body = []
+	req.on('data',(chunk) =>{
+		body.push(chunk);
+	}).on('end', () => {
+		body = Buffer.concat(body).toString();
+		console.log(body)
+		csvConverter(body)
+	})
+})
+
+
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
+
 
